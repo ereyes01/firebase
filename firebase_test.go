5b0df456aed5c67ee092145972f53bcde5dbc9af
Justin Tulloss <justin.tulloss@gmail.com>
2014-12-22 11:46:53 -0500

    Update for go 1.4 since circleci seems to support it now

--- firebase_test.go
+++ firebase_test.go
@@ -1,6 +1,7 @@
 package firebase_test
 
 import (
+	"fmt"
 	"os"
 	"reflect"
 	"testing"
@@ -158,11 +159,10 @@ func TestSetRules(t *testing.T) {
 	}
 }
 
-/* TODO: Once circle ci supports go 1.4, uncomment this
 func TestMain(m *testing.M) {
 	if testUrl == "" || testAuth == "" {
 		fmt.Printf("You need to set FIREBASE_TEST_URL and FIREBASE_TEST_AUTH\n")
 		os.Exit(1)
 	}
 	os.Exit(m.Run())
-}*/
+}
